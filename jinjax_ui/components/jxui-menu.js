5b4b17d24d117b53855bf7549826c6a787109a66
Juan-Pablo Scaletti <juanpablo@jpscaletti.com>
2024-06-04 21:58:38 -0500

    ...

--- jinjax_ui/components/jxui-menu.js
+++ jinjax_ui/components/jxui-menu.js
@@ -5,24 +5,15 @@
  */
 import { on } from "./jxui.js";
 
-const ATTR_TARGET = "data-menu-target";
-const ATTR_CLOSE_DELAY = "data-close-delay";
-const ATTR_ARIA_EXPANDED = "aria-expanded";
-
 const CLASS_OPEN = "ui-open";
 const CLASS_OPENED = "ui-opened";
 const CLASS_DISABLED = "ui-disabled";
 
-const SEL_BODY= "body";
-const SEL_BUTTON = `[${ATTR_TARGET}]`;
 const SEL_MENU = ".ui-menu";
 const SEL_MENU_OPEN = `${SEL_MENU}.${CLASS_OPEN}`;
 const SEL_ITEM = ".ui-menu-item"
 const SEL_ITEM_ENABLED = `${SEL_ITEM}:not(.${CLASS_DISABLED})`;
 
-const KEY_ESC = "Escape";
-const KEY_SPACE = " ";
-const KEY_ENTER = "Enter";
 const KEY_ARROW_DOWN = "ArrowDown";
 const KEY_ARROW_UP = "ArrowUp";
 const KEYS = [
@@ -32,53 +23,10 @@ const KEYS = [
   KEY_ARROW_DOWN,
 ];
 
-on("click", SEL_BUTTON, handleClickOnButton);
-on("keydown", SEL_BUTTON, handleButtonKeyDown);
 on("keydown", SEL_MENU, handleMenuKeyDown);
-
 on("click", SEL_ITEM, handleItemClick);
 on("mouseover", SEL_ITEM_ENABLED, handleItemMouseOver, 0);
 on("mouseleave", SEL_MENU, handleMenuMouseLeave, 0);
-
-on("keydown", SEL_BODY, closeAllOnEsc);
-on("mousedown", SEL_BODY, closeAll);
-on("mousedown", SEL_MENU, stopEvent);
-
-function handleClickOnButton(event, button) {
-  stopEvent(event)
-  const target = button.getAttribute(ATTR_TARGET);
-  const menu = document.getElementById(target);
-  menu.openedBy = button;
-  toggleMenu(menu);
-}
-
-function handleButtonKeyDown(event, button) {
-  if (!KEYS.includes(event.key)) return;
-
-  const target = button.getAttribute(ATTR_TARGET);
-  const menu = document.getElementById(target);
-  menu.openedBy = button;
-
-  switch (event.key) {
-    case KEY_SPACE:
-    case KEY_ENTER:
-      event.preventDefault()
-      toggleMenu(menu);
-      return;
-
-    case KEY_ARROW_DOWN:
-      event.preventDefault()
-      event.stopPropagation();
-      selectNextItem(menu);
-      return;
-
-    case KEY_ARROW_UP:
-      event.preventDefault()
-      event.stopPropagation();
-      selectPrevItem(menu);
-      return;
-  }
-}
 
 function handleMenuKeyDown(event, menu) {
   if (!KEYS.includes(event.key)) return;
@@ -124,75 +72,16 @@ function handleItemMouseOver(event, item) {
   selectItem(menu, item)
 }
 
-function closeAllOnEsc(event) {
-  if (event.key !== KEY_ESC) { return; }
-  closeAll();
-}
+function closeMenu(menu) {
 
-function closeAll() {
-  document
-    .querySelectorAll(SEL_MENU_OPEN)
-    .forEach(function(menu){
-      closeMenu(menu);
-    });
-}
-
-function stopEvent(event) {
-  event.stopPropagation();
-  event.preventDefault();
-  return false;
-}
-
-export function toggleMenu(menu) {
-  if (menuIsOpen(menu)) {
-    closeMenu(menu);
-  } else {
-    openMenu(menu);
-  }
 }
 
 export function menuIsOpen(menu) {
   return menu.classList.contains(CLASS_OPEN);
 }
 
-export function openMenu(menu) {
-  menu.classList.add(CLASS_OPEN);
-  setTimeout(function(){
-    menu.classList.add(CLASS_OPENED);
-  }, 0)
-
-  // Update button(s) state
-  document
-    .querySelectorAll(`[${ATTR_TARGET}="${menu.id}"]`)
-    .forEach(function(button){
-      button.setAttribute(ATTR_ARIA_EXPANDED, "true");
-    });
-}
-
-export function closeMenu(menu) {
-  const delay = +(menu.getAttribute(ATTR_CLOSE_DELAY) || "0");
-  if (isNaN(delay)) {
-    console.error("Invalid delay", delay);
-    return;
-  }
-  menu.classList.remove(CLASS_OPENED);
-
-  if (menu.openedBy) {
-    menu.openedBy.focus();
-  } else if (menu.selected) {
-    menu.selected.blur();
-  }
-  menu.openedBy = null;
-  menu.selected = null;
-
-  setTimeout(function(){
-    menu.classList.remove(CLASS_OPEN);
-    document
-      .querySelectorAll(`[${ATTR_TARGET}="${menu.id}"]`)
-      .forEach(function(button){
-        button.removeAttribute(ATTR_ARIA_EXPANDED);
-      });
-  }, delay)
+export function menuIsClosed(menu) {
+  return !menuIsOpen(menu);
 }
 
 export function selectNextItem(menu) {
@@ -248,8 +137,4 @@ export function selectPrevItem(menu) {
 export function selectItem(menu, item) {
   item.focus();
   menu.selected = item;
-}
-
-export function menuIsClosed(menu) {
-  return !menuIsOpen(menu);
 }
